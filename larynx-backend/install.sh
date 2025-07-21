f5d9b6bd9e19412c67385d2dc71740f5a346739b
NygerianPrynce <fadhillawal06@gmail.com>
2025-07-20 23:35:35 -0700

    Added instructions to download requirements

--- larynx-backend/install.sh
+++ larynx-backend/install.sh
@@ -2,3 +2,6 @@
 pip install -r requirements.txt --force-reinstall --no-deps talon
 pip install talon==1.4.4 --no-deps
 pip install lxml regex numpy scipy scikit-learn cssselect html5lib six
+#INSTRUCTIONS TO DOWNLOAD
+#chmod +x install.sh
+#./install.sh
